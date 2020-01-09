4e86f3e4977e69a9e3f5b5e2ac3d0459329e972c
Keliang Liu <keliangliu1@gmail.com>
2020-01-10 01:46:59 +1100

    change color at the end of countdown

--- src/App.js
+++ src/App.js
@@ -38,6 +38,8 @@ function App() {
   const [type, setType] = useState('Session');  // 'Session' || 'Break'
   const [intervalID, setIntervalID] = useState('');
   const [displayTimeLeft, setDisplayTimeLeft] = useState('25:00');
+  const [bgColor, setBgColor] = useState('white');
+
   const audio = document.getElementById("beep");
   useEffect(() => {
     let min = Math.floor(remainSeconds / 60);
@@ -57,6 +59,7 @@ function App() {
     setStatus('stop');
     setType('Session');
     setIntervalID('');
+    setBgColor('white');
     audio.pause();
     audio.currentTime = 0; // be rewound to the beginning
   }
@@ -81,13 +84,18 @@ function App() {
       setStatus('running');
       let counter = remainSeconds;
       let typeStr = type;
+      let color = bgColor
       const id = setInterval(() => {
         counter--;
         if (counter < 0) {
+          setBgColor('white');
           audio.play();
           let obj = changeType(typeStr);
           counter = obj.counter;
           typeStr = obj.typeStr;
+        } if (0 <= counter && counter <= 30) {
+          color = changeColor(color);
+          setRemainSeconds(counter);
         } else {
           setRemainSeconds(counter);
         }
@@ -96,6 +104,15 @@ function App() {
     } else {
       setStatus('stop');
       clearInterval(intervalID);
+    }
+  }
+  const changeColor = (_color) => {
+    if (_color === 'white' || _color === 'lightgreen') {
+      setBgColor('red');
+      return 'red';
+    } else {
+      setBgColor('lightgreen');
+      return 'lightgreen';
     }
   }
   const changeType = (str) => {
@@ -110,77 +127,79 @@ function App() {
     }
   }
   return (
-    <Container className={classes.root} maxWidth="sm">
-      <Typography variant="h5">
-        <span id='timer-label'>{type}</span> Time Countdown
+    <div style={{ width: '100%', height: '100vh', backgroundColor: `${bgColor}` }}>
+      <Container className={classes.root} maxWidth="sm">
+        <Typography variant="h5">
+          <span id='timer-label'>{type}</span> Time Countdown
       </Typography>
-      {type === 'Session' ? (
-        <div><DirectionsBikeIcon fontSize='large' /></div>
-      ) : (
-          <div><FreeBreakfastIcon fontSize='large' /></div>
-        )}
-      <Typography variant="h5">
-        <span id='time-left'>{displayTimeLeft}</span><br />
-      </Typography>
-      <span>min&nbsp;&nbsp;sec</span>
-      <Divider className={classes.divider} />
+        {type === 'Session' ? (
+          <div><DirectionsBikeIcon fontSize='large' /></div>
+        ) : (
+            <div><FreeBreakfastIcon fontSize='large' /></div>
+          )}
+        <Typography variant="h5">
+          <span id='time-left'>{displayTimeLeft}</span><br />
+        </Typography>
+        <span>min&nbsp;&nbsp;sec</span>
+        <Divider className={classes.divider} />
 
-      <Grid container>
-        <Grid item className={classes.sessionControl} xs={12} sm={6}>
-          <Typography id='session-label'>Session Time Length</Typography>
-          <ButtonGroup variant="contained" aria-label="split button">
-            <Button disabled style={{ color: 'black' }}>
-              <DirectionsBikeIcon />
+        <Grid container>
+          <Grid item className={classes.sessionControl} xs={12} sm={6}>
+            <Typography id='session-label'>Session Time Length</Typography>
+            <ButtonGroup variant="contained" aria-label="split button">
+              <Button disabled style={{ color: 'black' }}>
+                <DirectionsBikeIcon />
+              </Button>
+              <Button className={classes.textBtn} variant='text' disabled style={{ color: 'black' }}>
+                <span id='session-length'>{sessionLength}</span>&nbsp;min
             </Button>
-            <Button className={classes.textBtn} variant='text' disabled style={{ color: 'black' }}>
-              <span id='session-length'>{sessionLength}</span>&nbsp;min
+              <Button id='session-decrement' aria-label="decrement" onClick={handleSessionLength} value='-1'>
+                <RemoveIcon />
+              </Button>
+              <Button id='session-increment' aria-label="increment" onClick={handleSessionLength} value='1'>
+                <AddIcon />
+              </Button>
+            </ButtonGroup>
+          </Grid>
+          <Grid item className={classes.breakControl} xs={12} sm={6}>
+            <Typography id='break-label'>Break Time Length</Typography>
+            <ButtonGroup variant="contained" aria-label="split button">
+              <Button disabled style={{ color: 'black' }}>
+                <FreeBreakfastIcon />
+              </Button>
+              <Button className={classes.textBtn} variant='text' disabled style={{ color: 'black' }}>
+                <span id='break-length'>{breakLength}</span>&nbsp;min
             </Button>
-            <Button id='session-decrement' aria-label="decrement" onClick={handleSessionLength} value='-1'>
-              <RemoveIcon />
-            </Button>
-            <Button id='session-increment' aria-label="increment" onClick={handleSessionLength} value='1'>
-              <AddIcon />
-            </Button>
-          </ButtonGroup>
-        </Grid>
-        <Grid item className={classes.breakControl} xs={12} sm={6}>
-          <Typography id='break-label'>Break Time Length</Typography>
-          <ButtonGroup variant="contained" aria-label="split button">
-            <Button disabled style={{ color: 'black' }}>
-              <FreeBreakfastIcon />
-            </Button>
-            <Button className={classes.textBtn} variant='text' disabled style={{ color: 'black' }}>
-              <span id='break-length'>{breakLength}</span>&nbsp;min
-            </Button>
-            <Button id='break-decrement' aria-label="decrement" onClick={handleBreakLength} value='-1'>
-              <RemoveIcon />
-            </Button>
-            <Button id='break-increment' aria-label="increment" onClick={handleBreakLength} value='1'>
-              <AddIcon />
-            </Button>
-          </ButtonGroup>
-        </Grid>
-        <Grid item className={classes.playControl} xs={12} sm={12}>
-          <Typography>&nbsp;</Typography>
-          <ButtonGroup variant="contained" aria-label="split button">
-            <Button id='start_stop' onClick={startOrPause}>
-              <PlayArrowIcon />
-              <PauseIcon />
-            </Button>
-            <Button id='reset' onClick={reset}>
-              <StopIcon />
-              <RefreshIcon />
-            </Button>
-          </ButtonGroup>
-        </Grid>
-        <div className={classes.developer}>
-          <Typography variant="body2" align='center'>
-            Designed and Coded By Keliang Liu
+              <Button id='break-decrement' aria-label="decrement" onClick={handleBreakLength} value='-1'>
+                <RemoveIcon />
+              </Button>
+              <Button id='break-increment' aria-label="increment" onClick={handleBreakLength} value='1'>
+                <AddIcon />
+              </Button>
+            </ButtonGroup>
+          </Grid>
+          <Grid item className={classes.playControl} xs={12} sm={12}>
+            <Typography>&nbsp;</Typography>
+            <ButtonGroup variant="contained" aria-label="split button">
+              <Button id='start_stop' onClick={startOrPause}>
+                <PlayArrowIcon />
+                <PauseIcon />
+              </Button>
+              <Button id='reset' onClick={reset}>
+                <StopIcon />
+                <RefreshIcon />
+              </Button>
+            </ButtonGroup>
+          </Grid>
+          <div className={classes.developer}>
+            <Typography variant="body2" align='center'>
+              Designed and Coded By Keliang Liu
           </Typography>
-        </div>
-      </Grid>
-      <audio id="beep" preload="auto" src="https://goo.gl/65cBl1" />
-    </Container>
+          </div>
+        </Grid>
+        <audio id="beep" preload="auto" src="https://goo.gl/65cBl1" />
+      </Container>
+    </div>
   );
 }
 export default App;
